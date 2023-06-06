6296490c739b97e7e63ebe90acca3420566657dd
jakebroffman <jake.broffman1088@gmail.com>
2023-06-06 16:16:09 -0400

    commit tree b8f1fc7f1b4bf2f84bb03513461c61854e189472

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
-def countdown
+
+def countdown(seconds = 10)
+    
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds = 5)
+
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        sleep(1.0)
+    end
+    "HAPPY NEW YEAR!"
 end
